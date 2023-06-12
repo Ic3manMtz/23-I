628a8b67822f2c016731bdaceb7efacb65beeaf2
Ic3manKraken <molap96@gmail.com>
2023-06-12 08:45:11 -0600

    Reporte Practica 8
    Creación del PDF para la práctica 8 de Programación Concurrente

--- Programacion Concurrente/Practica 8/Ejemplos/EjemploAllgather.c
+++ Programacion Concurrente/Practica 8/Ejemplos/EjemploAllgather.c
@@ -16,7 +16,7 @@ void emisor_receptor(int mi_id, int num_proc)
     MPI_Allgather(&datosOut,1,MPI_INT,datosIn,1, MPI_INT,MPI_COMM_WORLD);
 
     for(i=0; i<num_proc; i++)
-      printf("Proceso %d: recibi datosIn[%d]=%d\n", mi_id,i,datosIn[i]);
+        printf("Proceso %d: recibi datosIn[%d]=%d\n", mi_id,i,datosIn[i]);
 
 
 }
@@ -25,12 +25,12 @@ void emisor_receptor(int mi_id, int num_proc)
 int main(int argc, char *argv[])
 {
     int rank, size;
- 
+
     MPI_Init(&argc, &argv); // Inicializacion del entorno MPI
 
     MPI_Comm_size(MPI_COMM_WORLD, &size); // Obtenemos el numero de procesos en el comunicador global
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Obtenemos la identificacion de nuestro proceso en el comunicador global
- 
+
     emisor_receptor(rank, size);
 
     MPI_Finalize();
